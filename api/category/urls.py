7d7fa8134fa33a283dc741d1389c3f2fff996603
SAM <85979367+Mosi-azizi@users.noreply.github.com>
2022-07-20 14:11:27 +0430

    create apps

--- api/category/urls.py
+++ api/category/urls.py
@@ -1,5 +1,9 @@
+from rest_framework import routers
+from . import views
 from django.urls import path,include
 
+router = routers.DefaultRouter()
+router.register(r'', views.CategoryViewSet)
 urlpatterns = [
-    path('category/',)
+    path('',include(router.urls))
 ]
--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home, name='api.home')
+    path('',views.home, name='api.home'),
+    path('category/',include('api.category.urls'))
 ]
